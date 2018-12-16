e27b809db56701dbed738283152ccf5e8fa2ccaa
Pradeep Madubashena Gedara <44329815+madubashena89@users.noreply.github.com>
2018-12-16 00:07:21 -0600

    Area and perimeter calculator
    This program can identify any two numbers you give whether it's a rectangle or a Square and give output of area and the perimeter of the shape.

---
+++ area_perimeter_calculator.py
@@ -0,0 +1,22 @@
+
+num1 = float(input("Enter 1st value of the shape:"))
+num2 = float (input("Enter the 2nd value of the shape:"))
+
+
+if num1 == num2:
+    print("Area of Square is: ", num1 * num2)
+    print("Perimeter of Square is: ", num1 * 4)
+
+else:
+    print("Area of Rectangle is : ", num1 * num2)
+    print("Perimeter of Rectangle is : ", num1 * 2 + num2 * 2)
+
+
+
+
+
+
+
+
+
+
